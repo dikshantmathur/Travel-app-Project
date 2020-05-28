dcbf9067206e23ca9d4284a17c13d8feee403979
dikshantmathur <dikshant.17jice038@jietjodhpur.ac.in>
2020-05-28 19:08:30 +0530

    Travel App For All

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  // All imported modules in your tests should be mocked automatically
+
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+
+  coverageDirectory: "coverage",
+
+
+};
